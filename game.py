8a207d3f83b672990ef3a611a9a31a6f788a0701
Gerald Pellegrino <geraldpellegrino061212@gmail.com>
2023-02-21 20:44:08 +0100

    création d'une fenetre à la bonne taille

--- game.py
+++ game.py
@@ -1,10 +1,18 @@
 import tkinter as tk 
+from utils.entreprise import Entreprise
+
+HEIGHT = 700
+WIDTH = 700
 
 
 class App(tk.Tk):
 
-    def __init__(self):
+    def __init__(self, entreprise: bool|Entreprise = False):
 
         super().__init__()
+        
+        self.config(bg="grey", width=WIDTH, height=HEIGHT)
 
-    
+
+    def tour(self):
+        pass
